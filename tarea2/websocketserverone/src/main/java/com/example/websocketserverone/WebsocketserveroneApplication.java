cae478442b49ab90929fea9fc26393a5f4c8dcec
10919 <you@example.com>
2023-04-10 10:34:11 -0500

    modificado

---
+++ tarea2/websocketserverone/src/main/java/com/example/websocketserverone/WebsocketserveroneApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketserverone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketserveroneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketserveroneApplication.class, args);
+	}
+
+}
---
+++ tarea2/websocketserverone/src/test/java/com/example/websocketserverone/WebsocketserveroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketserverone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketserveroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
